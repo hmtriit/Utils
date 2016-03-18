700e2de4df50f068768e99a3f50195dba5edab53
liconrepo <khairul.alam.licon@framgia.com>
2016-03-18 08:12:43 +0700

    Apputils feature added

---
+++ utilslib/src/main/java/con/licon/data/IntentData.java
@@ -0,0 +1,16 @@
+package con.licon.data;
+
+/**
+ * Created by FRAMGIA\khairul.alam.licon on 18/3/16.
+ */
+public class IntentData {
+    // Share intent constants
+    public static final String SHARE_TYPE_PLAIN_TEXT = "text/plain";
+    public static final String SHARE_TYPE_MESSAGE = "message/rfc822";
+    public static final String SHARE_FILTER_KEY_TWITTER = "com.twitter.android";
+    public static final String SHARE_FILTER_KEY_GOOGLE_PLUS = "com.google.android.apps.plus";
+    public static final String SHARE_FILTER_KEY_FACEBOOK = "facebook";
+    public static final String SHARE_FILTER_KEY_LINKEDIN = "com.linkedin.android";
+    public static final String SHARE_FILTER_KEY_EMAIL = "android.email";
+    public static final String SHARE_FILTER_KEY_GMAIL = "android.gm";
+}
